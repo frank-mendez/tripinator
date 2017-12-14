097b967612d5e17cce4f14f896b4137bedeec6dd
Frank Mendez <Frank Mendez>
2017-12-14 12:15:44 +0800

    feat(initial): Initial commit

---
+++ tripinator.php
@@ -0,0 +1,9 @@
+<?php
+
+/*Plugin Name: Tripinator
+Plugin URI: https://github.com/frank-mendez/tripinator
+Description: A super simplified Phonebook
+Version: 1.0.0
+Author: Heaviside Group
+Author URI: https://www.heavisidegroup.com/
+License: GPLv2 or later.*/
